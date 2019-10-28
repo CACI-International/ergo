38775d8e0d2d16dfc60855e855261e44054910ff
Alex Franchuk <afranchuk@caci.com>
2019-10-28 15:10:31 -0600

    Remove grease procedure runtime.
    It's likely an unnecessary abstraction that can be mirrored using the
    Plan trait and (later) serialization/identification details.

--- grease/src/runtime/mod.rs
+++ grease/src/runtime/mod.rs
@@ -1,20 +1,14 @@
 //! Execution logic for plans.
 
 use std::fmt;
-use std::rc::Rc;
-
-use crate::plan::Value;
 
 mod command;
 mod log;
-mod procedure;
 mod task_manager;
 
 use self::log::EmptyLogTarget;
 pub use self::log::{logger_ref, Log, LogEntry, LogLevel, LogTarget, LoggerRef};
 pub use command::Commands;
-use procedure::EmptyResolver;
-pub use procedure::{Proc, Procedure, ProcedureFunc, ProcedureInput, Resolver};
 pub use task_manager::TaskManager;
 pub use futures::future;
 pub use futures::future::{FutureExt,TryFutureExt};
@@ -28,14 +22,11 @@ pub struct Context {
     pub cmd: Commands,
     /// The logging interface.
     pub log: Log,
-    /// The procedure interface.
-    proc: Rc<Proc>,
 }
 
 #[derive(Default)]
 pub struct ContextBuilder {
     logger: Option<LoggerRef>,
-    resolver: Option<Box<dyn Resolver>>,
 }
 
 #[derive(Debug)]
@@ -62,19 +53,11 @@ impl ContextBuilder {
         self
     }
 
-    pub fn procedure_resolver<R: Resolver + 'static>(mut self, resolver: R) -> Self {
-        self.resolver = Some(Box::new(resolver));
-        self
-    }
-
     pub fn build(self) -> Result<Context, BuilderError> {
         Ok(Context {
             task: TaskManager::new().map_err(BuilderError::TaskManagerError)?,
             cmd: Commands::new(),
             log: Log::new(self.logger.unwrap_or_else(|| logger_ref(EmptyLogTarget))),
-            proc: Rc::new(Proc::new(
-                self.resolver.unwrap_or_else(|| Box::new(EmptyResolver)),
-            )),
         })
     }
 }
@@ -82,11 +65,6 @@ impl ContextBuilder {
 impl Context {
     pub fn builder() -> ContextBuilder {
         Default::default()
-    }
-
-    pub fn run_proc<Input: ProcedureInput>(&mut self, input: Input) -> Result<Value, String> {
-        let proc = self.proc.clone();
-        proc.run(self, input)
     }
 }
 
@@ -134,56 +112,6 @@ mod tests {
         let output = TestRuntimePlan.plan(&mut ctx);
         let result = futures::executor::block_on(output)?;
         assert!(*result == vec![0, 1]);
-        Ok(())
-    }
-
-    fn a_resolve(_ctx: &mut Context, input: Value) -> Result<Value, String> {
-        Ok(crate::derived_value!(vec_type(), (a=input) {
-            a.map_ok(|av| {
-                let mut o = (*av).clone();
-                o.push(1);
-                o
-            })
-        }))
-    }
-
-    struct AData {
-        val: Value,
-    }
-
-    impl AData {
-        pub fn new(val: Value) -> AData {
-            AData { val }
-        }
-    }
-
-    impl Into<Value> for AData {
-        fn into(self) -> Value {
-            self.val
-        }
-    }
-
-    impl ProcedureInput for AData {
-        fn procedure() -> Procedure {
-            Uuid::from_bytes([1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
-        }
-    }
-
-    #[test]
-    fn procedure_lookup() -> Result<(), String> {
-        let mut procs: std::collections::HashMap<Procedure, ProcedureFunc> =
-            std::collections::HashMap::new();
-        procs.insert(AData::procedure(), a_resolve);
-
-        let mut ctx = Context::builder()
-            .procedure_resolver(procs)
-            .build()
-            .map_err(|e| format!("{}", e))?;
-
-        let output = ctx.run_proc(AData::new(Value::constant(vec_type(), vec![0])))?;
-        let result = futures::executor::block_on(output)?;
-        assert!(*result == vec![0, 1]);
-
         Ok(())
     }
 
