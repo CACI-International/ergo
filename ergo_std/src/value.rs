f38b7a58b2a54214c7ee4dce113d1d10befe7eda
Alex Franchuk <afranchuk@caci.com>
2020-09-18 00:00:34 -0400

    Fix value force implementation.
    Start to use ergo in the repository.

--- ergo_std/src/value.rs
+++ ergo_std/src/value.rs
@@ -19,96 +19,107 @@ pub fn module() -> Value {
 }
 
 fn force_fn() -> Value {
-    types::Function::new(|ctx| async move {
-        let val = ctx.args.next().ok_or("value not provided")?;
+    types::Function::new(|ctx| {
+        async move {
+            let val = ctx.args.next().ok_or("value not provided")?;
 
-        ctx.unused_arguments()?;
+            ctx.unused_arguments()?;
 
-        let (source, val) = val.take();
+            let (source, val) = val.take();
 
-        match ctx.traits.get::<ValueByContent>(&val) {
-            Some(t) => t
-                .value_by_content(&ctx.traits, val)
-                .map(|v| ctx.call_site.clone().with(v)),
-            None => Err(source
-                .with(format!(
-                    "ValueByContent not implemented for {}",
-                    type_name(&ctx.traits, &val.grease_type())
-                ))
-                .into_grease_error()),
+            match ctx.traits.get::<ValueByContent>(&val) {
+                Some(t) => {
+                    force_value_nested(&ctx.traits, val.clone()).await?;
+                    t.value_by_content(&ctx.traits, val)
+                        .map(|v| ctx.call_site.clone().with(v))
+                }
+                None => Err(source
+                    .with(format!(
+                        "ValueByContent not implemented for {}",
+                        type_name(&ctx.traits, &val.grease_type())
+                    ))
+                    .into_grease_error()),
+            }
         }
-    }.boxed())
+        .boxed()
+    })
     .into()
 }
 
 fn cache_fn() -> Value {
-    types::Function::new(|ctx| async move {
-        let v = ctx.args.next().ok_or("no argument to cache")?;
-        let args = std::mem::take(&mut ctx.args);
+    types::Function::new(|ctx| {
+        async move {
+            let v = ctx.args.next().ok_or("no argument to cache")?;
+            let args = std::mem::take(&mut ctx.args);
 
-        let store = ctx.store.item(item_name!("cache"));
-        let log = ctx.log.sublog("cache");
+            let store = ctx.store.item(item_name!("cache"));
+            let log = ctx.log.sublog("cache");
 
-        let to_cache = apply_value(ctx, v, args.unchecked(), true).await?.unwrap();
+            let to_cache = apply_value(ctx, v, args.unchecked(), true).await?.unwrap();
 
-        if let Some(_) = ctx.traits.get::<Stored>(&to_cache) {
-            let deps = depends![to_cache];
-            let traits = ctx.traits.clone();
-            Ok(ctx.call_site.clone().with(Value::new(
-                to_cache.grease_type(),
-                async move {
-                    let err = match read_from_store(&traits, &store, to_cache.id()) {
-                        Ok(val) => {
-                            log.debug(format!(
-                                "successfully read cached value for {}",
-                                to_cache.id()
-                            ));
-                            if val.grease_type() != to_cache.grease_type() {
-                                "cached value had different value type".into()
-                            } else {
-                                return Ok(val
-                                    .await
-                                    .expect("value should have success value from cache"));
+            if let Some(_) = ctx.traits.get::<Stored>(&to_cache) {
+                let deps = depends![to_cache];
+                let traits = ctx.traits.clone();
+                Ok(ctx.call_site.clone().with(Value::new(
+                    to_cache.grease_type(),
+                    async move {
+                        let err = match read_from_store(&traits, &store, to_cache.id()) {
+                            Ok(val) => {
+                                log.debug(format!(
+                                    "successfully read cached value for {}",
+                                    to_cache.id()
+                                ));
+                                if val.grease_type() != to_cache.grease_type() {
+                                    "cached value had different value type".into()
+                                } else {
+                                    return Ok(val
+                                        .await
+                                        .expect("value should have success value from cache"));
+                                }
                             }
+                            Err(e) => e,
+                        };
+                        log.debug(format!("failed to read cache entry, (re)caching: {}", err));
+                        force_value_nested(&traits, to_cache.clone()).await?;
+                        if let Err(e) = write_to_store(&traits, &store, to_cache.clone())
+                            .map_err(|e| e.to_string())
+                        {
+                            log.warn(format!("failed to cache value: {}", e));
                         }
-                        Err(e) => e,
-                    };
-                    log.debug(format!("failed to read cache entry, (re)caching: {}", err));
-                    force_value_nested(&traits, to_cache.clone()).await?;
-                    if let Err(e) =
-                        write_to_store(&traits, &store, to_cache.clone()).map_err(|e| e.to_string())
-                    {
-                        log.warn(format!("failed to cache value: {}", e));
-                    }
-                    Ok(to_cache
-                        .await
-                        .expect("error should have been caught previously"))
-                },
-                deps,
-            )))
-        } else {
-            Err("no stored trait implemented for value".into())
+                        Ok(to_cache
+                            .await
+                            .expect("error should have been caught previously"))
+                    },
+                    deps,
+                )))
+            } else {
+                Err("no stored trait implemented for value".into())
+            }
         }
-    }.boxed())
+        .boxed()
+    })
     .into()
 }
 
 fn variable_fn() -> Value {
-    types::Function::new(|ctx| async move {
-        let v = ctx.args.next().ok_or("no argument to variable")?;
+    types::Function::new(|ctx| {
+        async move {
+            let v = ctx.args.next().ok_or("no argument to variable")?;
 
-        let deps = if let Some(v) = ctx.args.kw("depends") {
-            depends![*v]
-        } else {
-            Default::default()
-        };
+            let deps = if let Some(v) = ctx.args.kw("depends") {
+                depends![*v]
+            } else {
+                Default::default()
+            };
 
-        let args = std::mem::take(&mut ctx.args);
-        ctx.unused_arguments()?;
+            let args = std::mem::take(&mut ctx.args);
+            ctx.unused_arguments()?;
 
-        let val = apply_value(ctx, v, args.unchecked(), true).await?;
+            let val = apply_value(ctx, v, args.unchecked(), true).await?;
 
-        Ok(val.map(|v| v.set_dependencies(deps)))
-    }.boxed())
+            Ok(val.map(|v| v.set_dependencies(deps)))
+        }
+        .boxed()
+    })
     .into()
 }
