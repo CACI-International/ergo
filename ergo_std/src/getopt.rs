d0b638d6e1fbeaaf9d613595d56feadce3265567
Alex Franchuk <afranchuk@caci.com>
2021-07-21 21:51:45 -0400

    Add a `consecutive` flag to `std:getopt` to only parse until the first non-flag argument.

--- ergo_std/src/getopt.rs
+++ ergo_std/src/getopt.rs
@@ -17,17 +17,31 @@ use std::collections::BTreeMap;
 ///
 /// Arguments: `(Array:Of :String) :args`
 ///
+/// Keyed Arguments:
+/// * `:consecutive` (optional) - If present, only parse consecutive flags at the beginning of the
+/// array, i.e., consider all arguments after the first positional argument to be positional.
+///
 /// Returns an Args type with the positional and keyed arguments.
-pub async fn function(args: types::Array) -> Value {
+pub async fn function(args: types::Array, (consecutive): [_]) -> Value {
     let args = args.to_owned().0;
+    let consecutive = consecutive.is_some();
 
     let mut positional = Vec::new();
     let mut keyed = BTreeMap::new();
 
+    let mut force_positional = false;
+
     for a in args {
         let s = try_result!(Context::eval_as::<types::String>(a).await);
+        if force_positional {
+            positional.push(s.into());
+            continue;
+        }
         match s.as_ref().as_str().strip_prefix("--") {
-            None => positional.push(s.into()),
+            None => {
+                positional.push(s.into());
+                force_positional = consecutive;
+            }
             Some(rest) => {
                 let src = Source::get(&s);
                 match rest.split_once("=") {
@@ -57,6 +71,10 @@ mod test {
             t.assert_content_eq("self:getopt [a,b,c,d]", "fn ^:args -> :args |> a b c d");
             t.assert_content_eq("self:getopt [--flag,pos1,'--key=a',pos2,pos3,'--key2=123=b']",
                 "fn ^:args -> :args |> ^flag (key=a) (key2='123=b') pos1 pos2 pos3");
+        }
+
+        fn consecutive(t) {
+            t.assert_content_eq("self:getopt ^consecutive [--a,--b,c,--d]", "fn ^:args -> :args |> ^a ^b c --d");
         }
     }
 }
