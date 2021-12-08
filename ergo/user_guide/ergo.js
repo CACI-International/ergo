e657819e777c1065e6d1a4ccb4226c33e43a483c
Alex Franchuk <afranchuk@caci.com>
2021-12-08 14:58:21 -0500

    Improve highlight.js syntax as well as user guide styles.

--- ergo/user_guide/ergo.js
+++ ergo/user_guide/ergo.js
@@ -1,25 +1,62 @@
+var string_merge = {
+    className: 'operator',
+    begin: /\^/
+};
+
+var backslash_escape = {
+    className: 'char.escape',
+    begin: /\\(n|t|\\|"|u\{[0-9a-fA-F]{1-6}\})/,
+    relevance: 0
+};
+
 /*
 Language: Ergo
 */
 function ergo_lang(hljs) {
     return {
         name: "Ergo",
-        keywords: "ergo pat fn index std workspace doc bind unset",
         contains: [
-            hljs.COMMENT(/#/, /$/, {
-                contains: [
-                    {
-                        className: 'doc', begin: /##/, end: /$/
-                    }
-                ]
-            }),
-            hljs.QUOTE_STRING_MODE,
+            hljs.COMMENT(/# /, /$/),
+            {
+                className: 'attribute',
+                begin: /## /,
+                end: /$/,
+                contains: [string_merge]
+            },
+            {
+                className: 'built_in',
+                begin: /\b(ergo|pat|fn|index|std|workspace|doc|bind|unset)\b/
+            },
+            {
+                className: 'string',
+                begin: '"',
+                end: '"',
+                contains: [string_merge, backslash_escape]
+            },
+            {
+                className: 'string',
+                begin: /' /,
+                end: /$/,
+                contains: [string_merge]
+            },
             {
                 className: 'operator',
                 begin: /(:|\^|!|->|=)/
             },
             {
-                className: 'keyword',
+                className: 'punctuation',
+                begin: /(\(|\)|\{|\}|\[|\]|,|;)/
+            },
+            {
+                className: 'attribute',
+                begin: /##/
+            },
+            {
+                className: 'comment',
+                begin: /#/
+            },
+            {
+                className: 'meta',
                 begin: /(\|>?|<\|)/
             }
         ]
